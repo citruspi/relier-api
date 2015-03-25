56c420bf206eed5f45b25527c25d03c98d21fe83
Mihir Singh <git.service@mihirsingh.com>
2015-03-25 13:28:44 -0500

    Defined a method in the invitation model to generate tokens

--- relier/models/invitation.py
+++ relier/models/invitation.py
@@ -1,6 +1,19 @@
 from peewee import *
+import string
 from . import database
 from organization import Organization
+import random
+
+def generate_token():
+
+    token = ''
+    length = 10
+    pool = string.ascii_uppercase + string.digits
+
+    for _ in range(10):
+        token += random.SystemRandom().choice(pool)
+
+    return token
 
 class Invitation(Model):
 
