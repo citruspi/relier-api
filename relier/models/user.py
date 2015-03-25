8e2aa2174233eb262ebfe2ebb1b655dfe7bf970c
Mihir Singh <git.service@mihirsingh.com>
2015-03-25 15:20:48 -0500

    Implemented a method to create a user from an invitation

--- relier/models/user.py
+++ relier/models/user.py
@@ -1,6 +1,9 @@
 from peewee import *
 from . import database
 from organization import Organization
+from invitation import Invitation
+import requests
+import bcrypt
 
 class User(Model):
 
@@ -14,6 +17,41 @@ class User(Model):
     can_answer = BooleanField(default=False)
     timezone = CharField()
     html_email = BooleanField(default=True)
+
+    @staticmethod
+    def from_invitation(firstname, lastname, password, address, invitation):
+
+        invitation = Invitation.get(Invitation.id == invitation)
+
+        geoip_endpoint = 'https://freegeoip.net/json/{address}'
+        geoip_endpoint = geoip_endpoint.format(address = address)
+
+        timezone = None
+
+        r = requests.get(geoip_endpoint)
+
+        if r.status_code == 200:
+
+            try:
+                timezone = r.json()['time_zone']
+            except KeyError:
+                pass
+
+        if timezone is None:
+            timezone = 'America/Chicago'
+
+        password = bcrypt.hashpw(password, bcrypt.gensalt())
+
+        user = User.create(organization = invitation.organization,
+                            firstname = firstname,
+                            lastname = lastname,
+                            email = invitation.email,
+                            password = password,
+                            timezone = timezone)
+
+        invitation.delete_instance()
+
+        return user
 
     class Meta:
 
