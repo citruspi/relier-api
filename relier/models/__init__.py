caa28927fe427120ad48977ddee1b4b379814098
Mihir Singh <git.service@mihirsingh.com>
2015-03-25 13:07:24 -0500

    Automatically created the Invitation table

--- relier/models/__init__.py
+++ relier/models/__init__.py
@@ -4,5 +4,6 @@ database = PostgresqlDatabase('relier')
 
 from organization import Organization
 from user import User
+from invitation import Invitation
 
-database.create_tables([Organization, User], True)
+database.create_tables([Organization, User, Invitation], True)
