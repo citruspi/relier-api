699f176521dbc32854d493313f944973b2bb5f9b
Mihir Singh <git.service@mihirsingh.com>
2015-03-25 20:28:00 -0500

    Defined an invitation resource for creating and listing invitations

--- relier/api/__init__.py
+++ relier/api/__init__.py
@@ -3,9 +3,11 @@ from flask.ext import restful
 from organization import OrganizationResource
 from authentication import TokenResource, AuthenticatedResource
 from user import UserInstance
+from invitation import InvitationResource
 
 app = Flask(__name__)
 api = restful.Api(app)
 
 api.add_resource(OrganizationResource, '/organizations/')
 api.add_resource(TokenResource, '/tokens/')
+api.add_resource(InvitationResource, '/invitations/')
---
+++ relier/api/invitation.py
@@ -0,0 +1,40 @@
+from relier.api import AuthenticatedResource
+from flask import abort, request, g, make_response
+from relier.models import Invitation, User
+
+class InvitationResource(AuthenticatedResource):
+
+    def post(self):
+
+        if not g.user.is_admin:
+            abort(403)
+
+        try:
+            email = request.json['email_address']
+        except KeyError:
+            abort(400)
+
+        if User.exists(email):
+            abort(409)
+
+        try:
+            invitation = Invitation.send(g.user.organization, email)
+        except:
+            abort(500)
+
+        response = make_response('', 201)
+        response.headers['Location'] = '/invitations/{id_}/'.format(
+                                                        id_ = invitation.id)
+        return response
+
+    def get(self):
+
+        if not g.user.is_admin:
+            abort(403)
+
+        query = Invitation.select().where(
+                                Invitation.organization == g.user.organization)
+
+        invitations = [{'email': invitation.email} for invitation in query]
+
+        return invitations
