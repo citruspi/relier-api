791a79e0c3941262d8c991c0c292bbd3c9c73047
Mihir Singh <git.service@mihirsingh.com>
2015-03-25 21:30:49 -0500

    Removed the field linking answers and questions to users

--- relier/models/answer.py
+++ relier/models/answer.py
@@ -1,17 +1,14 @@
 from peewee import *
 from . import database
-
-from user import User
 from question import Question
 
 class Answer(Model):
 
-    user = ForeignKeyField(User, related_name='answers')
     question = ForeignKeyField(Question, related_name='answers')
     created = DateTimeField()
     updated = DateTimeField()
     content = CharField()
-    
+
     class Meta:
 
         database = database
--- relier/models/question.py
+++ relier/models/question.py
@@ -1,11 +1,9 @@
 from peewee import *
 from . import database
-from user import User
 from event import Event
 
 class Question(Model):
-    
-    user = ForeignKeyField(User, related_name='questions')
+
     event = ForeignKeyField(Event, related_name='questions')
     created = DateTimeField()
     updated = DateTimeField()
